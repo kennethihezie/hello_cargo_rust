951c98fffdb870be9d6d023e6b89c1659ec94576
kennethihezie <kennethihezie06@gmail.com>
2024-05-06 21:01:11 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// We can build a project without producing a binary to check for errors using cargo check
+// When your project is finally ready for release, you can use cargo build --release
